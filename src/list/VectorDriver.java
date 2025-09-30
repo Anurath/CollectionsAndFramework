dbb4c819da77c9ec9ab9fd471cb2808a344335a8
anurath <anurathwaghmode2018@gmail.com>
2025-09-30 23:12:16 +0530

    Vector methods added

--- src/list/VectorDriver.java
+++ src/list/VectorDriver.java
@@ -21,8 +21,7 @@ public class VectorDriver {
 		System.out.println(v1);
 		v1.add(10);
 		
-		//Capacity gets increased by 2x
-		System.out.println(v1.capacity());
+		System.out.println(v1.capacity()); //Capacity gets increased by 2x
 		
 		System.out.println("----------------------------------------------------------------------------------");
 		
@@ -35,6 +34,38 @@ public class VectorDriver {
 		v2.add(10);
 		System.out.println(v2.capacity());
 		v2.add(10);
-		System.out.println(v2.capacity());
+		System.out.println(v2.capacity()); // capacity + 2 = 7 
+		
+		v1.addElement(90);
+		
+		v2.setElementAt(v2, 0);
+		
+		System.out.println("--------------------------------------------------------------------------------------------");
+		
+		
+		Vector v3 = new Vector();
+		
+		v3.addElement(11);
+		v3.addElement(22);
+		v3.addElement(33);
+		v3.add(44);
+		
+		v3.remove((Integer)44);
+		v3.removeElement(11);
+		
+		System.out.println(v3);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
