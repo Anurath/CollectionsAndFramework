6efc0e59e0d03af8e387c37960967f7bfe34a45d
anurath <anurathwaghmode2018@gmail.com>
2025-10-07 14:07:57 +0530

    New Custom Student class created

--- src/queue/QueueDriver.java
+++ src/queue/QueueDriver.java
@@ -5,17 +5,27 @@ import java.util.PriorityQueue;
 public class QueueDriver {
 
 	public static void main(String[] args) {
-		PriorityQueue p1 = new PriorityQueue();
+//		PriorityQueue p1 = new PriorityQueue();
+//		
+//		p1.add(70);
+//		p1.add(50);
+//		p1.add(100);
+//		p1.add(30);
+//		p1.add(90);
+//		p1.add(60);
+//		
+//		System.out.println(p1);
 		
-		p1.add(70);
-		p1.add(50);
-		p1.add(100);
-		p1.add(30);
-		p1.add(90);
-		p1.add(60);
+		
+		
+		PriorityQueue<Student> p1 = new PriorityQueue<Student>();
+		
+		p1.add(new Student("Karan", 22, "PK", 12));
+		p1.add(new Student("Anurath", 21, "BK", 13));
+		p1.add(new Student("Aniket", 23, "DB", 15));
+		p1.add(new Student("Anjali", 21, "ZP", 17));
+		p1.add(new Student("Anuradha", 20, "DZ", 3));
 		
 		System.out.println(p1);
-		
-		
 	}
 }
--- src/queue/Student.java
+++ src/queue/Student.java
@@ -1,5 +1,35 @@
 package queue;
 
-public class Student {
+public class Student implements Comparable<Student> {
 
+	String name;
+	int age;
+	String college;
+	int stid;
+	public Student(String name, int age, String college, int stid) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.college = college;
+		this.stid = stid;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + ", college=" + college + ", stid=" + stid + "]";
+	}
+	@Override
+	public int compareTo(Student o) {
+		if(this.name.compareTo(o.name)>0)
+		{
+			return 1;
+		}
+		else if(this.name.compareTo(o.name)==0)
+		{
+			return 0;
+		}
+		return -1;
+	}
+	
+	
+	
 }
